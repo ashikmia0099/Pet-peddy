2338c84ee3310ae764e6a15d59f22ce64c68fb49
Ashik Mia <ashikmia5686@gmail.com>
2024-10-12 14:58:36 +0600

    full frontend design

---
+++ js/home.js
@@ -0,0 +1,27 @@
+
+// // show all card data
+
+// const DisplayCard =async () => {
+
+//     try{
+//         const Card_data = await fetch('https://openapi.programming-hero.com/api/peddy/pets')
+//         const data = await Card_data.json()
+//         console.log(data)
+//         DisplayCardData(data.pets)
+//     }
+        
+
+//     catch(error){
+//         console.log("Error feching user",error)
+
+//     }
+// }
+
+// DisplayCard()
+
+
+// const DisplayCardData = (cardData) => {
+
+//     const cardContainer = document.getElementById('')
+
+// }
